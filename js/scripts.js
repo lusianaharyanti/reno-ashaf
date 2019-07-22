a000c8157d4b36650930cfbdcbdaf1e9f7aa47b1
lusiana <haryanti.lusiana@gmail.com>
2019-07-22 17:42:56 +0700

    addnew component

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 
 window.mr = window.mr || {};
 
+const cursor = document.querySelector('.cursor');
+
+        document.addEventListener('mousemove', e => {
+            cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+        })
+
+        document.addEventListener('click', () => {
+            cursor.classList.add("expand");
+
+            setTimeout(() => {
+                cursor.classList.remove("expand");
+            }, 500)
+        })
+
 mr = (function (mr, $, window, document){
     "use strict";
 
